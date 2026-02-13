422143fd08acad98b2527ce0bb9670e91eeefa60
Bowen Zhang <2042866313@qq.com>
2026-02-13 11:01:02 +0800

    feat: add TaskInfo definition

--- os/proc.h
+++ os/proc.h
@@ -4,7 +4,7 @@
 #include "types.h"
 
 #define NPROC (16)
-
+#define MAX_SYSCALL_NUM (500)
 // Saved registers for kernel context switches.
 struct context {
 	uint64 ra;
@@ -43,6 +43,19 @@ struct proc {
 /*
 * LAB1: you may need to define struct for TaskInfo here
 */
+typedef enum {
+    UnInit,
+    Ready,
+    Running,
+    Exited,
+} TaskStatus;
+
+struct TaskInfo
+{
+	TaskStatus status;
+	unsigned int syscall_times[MAX_SYSCALL_NUM];
+	int time;
+};
 
 struct proc *curr_proc();
 void exit(int);
